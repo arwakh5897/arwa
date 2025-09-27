d9bbdbd4f990ce7d3aa780e815f5ffda15c5ebc8
zain929292 <za735232@gmail.com>
2025-09-27 17:08:05 +0500

    backend connected

--- backend/api/contact.js
+++ backend/api/contact.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://za735232_db_user:Zain6970@contact-cluster.uovpomi.mongodb.net/?retryWrites=true&w=majority&appName=contact-cluster";
 const client = new MongoClient(uri);
 
 export default async function handler(req, res) {
